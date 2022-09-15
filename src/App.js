2b15a163d85216c06c42d7105891e63b28d43ad5
MR-DHRUV <dhruvgupta742@gmail.com>
2022-09-15 18:42:06 +0530

    Init

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import BottomNav from './Components/BottomNav';
+import Sidebar from './Components/Sidebar';
 
 function App() {
   return (
     <div className="App">
-
+      {window.innerWidth > 750 ? <Sidebar /> : <BottomNav />}
     </div>
   );
 }
