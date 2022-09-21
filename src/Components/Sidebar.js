eef9a5754bd9cdb139187f9ed43a6e7e1f711b9d
MR-DHRUV <dhruvgupta742@gmail.com>
2022-09-21 20:12:50 +0530

    Side nav

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,8 +1,31 @@
 import React from 'react'
+import { BsFillMegaphoneFill } from "react-icons/bs";
+import { FaBlogger } from "react-icons/fa";
+import { FaWpforms } from "react-icons/fa";
+import { FaLink } from "react-icons/fa";
+import { FaAward } from "react-icons/fa";
+import { BsFillPeopleFill } from "react-icons/bs";
+import { FaHandshake } from "react-icons/fa";
+import { FaPhoneSquareAlt } from "react-icons/fa";
+import { FaSmileBeam } from "react-icons/fa";
+import './Assests/CSS/sidebar.css'
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+
+    <div className='sidebar'>
+      <h3 className='navLinks logoNav'> <img src="https://usict.acm.org/assets/images/acm-logo.svg" alt="" width={32} /> Acm Admin</h3>
+      <a href='#' className='navLinks'><FaSmileBeam className='icons' /> [->  </a>
+      <a href='#' className='navLinks'><BsFillMegaphoneFill className='icons' />announcements</a>
+      <a href='#' className='navLinks'><FaBlogger className='icons' />Blogs</a>
+      <a href='#' className='navLinks'><FaWpforms className='icons' />Forms</a>
+      <a href='#' className='navLinks'><FaLink className='icons' />Links</a>
+      <a href='#' className='navLinks'><FaAward className='icons' />Certificates</a>
+      <a href='#' className='navLinks'><BsFillPeopleFill className='icons' />Links</a>
+      <a href='#' className='navLinks'><FaHandshake className='icons' />Links</a>
+      <a href='#' className='navLinks'><FaPhoneSquareAlt className='icons' />Links</a>
+
+    </div>
   )
 }
 
